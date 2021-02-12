a5a6c9858341285d2418c0853dfc4217555c6a27
anthonymarini <63125634+anthonymarini@users.noreply.github.com>
2021-02-12 16:39:46 +0200

    First commit

---
+++ HoroscopeGenerator/main.js
@@ -0,0 +1,6 @@
+/* Horoscope Generator (part of Codecademy's full stack training)
+   Author: Anthony Marini
+   
+   The goal is to generate random astrology readings composed of three different
+   parts: the theme, the sign, and the reading itself
+*/
