4db31c37fe112f9c732a87564c499e6d3b42643c
anthonymarini <63125634+anthonymarini@users.noreply.github.com>
2021-02-20 15:30:42 +0200

    Added comments

--- StyleGuide/scripts.js
+++ StyleGuide/scripts.js
@@ -1,12 +1,18 @@
-const LOG = false;
-let prev = null;
+const LOG = false; // make "true" to enable logging to the console
+let prev = null; // keeps track of the previous button clicked to avoid useless work
 
+/*  If a new radio button is clicked, then it makes two lists of objects:
+    first the items to be made visible, and second the items to be hidden.
+    Since NodeLists do not have a concat function, I had to go about it
+    a different way. */
 function changeColorSyntax() {
     let val = document.querySelector("input[name=colorRadios]:checked").value;
+    
     if ( prev !== val ) {
         log( val + " was chosen." );
         let changeVals = [];
         let otherVals = [];
+        
         switch( val ) {
             case "hex":
                 changeVals = document.getElementsByClassName("value hex");
@@ -32,6 +38,7 @@ function changeColorSyntax() {
             changeVals[i].style.display = "block";
         for ( let j=0; j<otherVals.length; j++ )
             otherVals[j].style.display = "none";
+        
         prev = val;
     }
 }
