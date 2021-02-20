1564ff8e643ae662357613f748df09e73af06d94
anthonymarini <63125634+anthonymarini@users.noreply.github.com>
2021-02-20 12:11:08 +0200

    First commit

---
+++ StyleGuide/scripts.js
@@ -0,0 +1,21 @@
+const LOG = true;
+
+function log( msg ) {
+    if ( LOG ) {
+        console.log( msg );
+    }
+}
+
+function changeColorValue( num ) {
+    log( "Number " + num + " was chosen." );
+    switch( num ) {
+        case 0: 
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        default: 
+            break;
+    }
+}
